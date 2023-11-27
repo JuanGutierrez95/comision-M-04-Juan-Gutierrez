a9c5ddc6d563d1f84446bf883e8e8d02af16bb35
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-27 14:12:30 -0300

    Se agregaron los modelos

---
+++ server/src/models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 200,
+  },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  comments: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
