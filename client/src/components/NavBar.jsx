6a028a420393ec2b96d7625ff6f051472d90f879
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-14 23:45:14 -0300

    Se trabajó en el componente NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../images/arg-programa.png";
+import { useAuthContext } from "../context/AuthContext";
 const NavBar = () => {
+  const { username, logout } = useAuthContext();
+
+  const handleLogoutChange = () => {
+    logout();
+  };
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -17,9 +24,18 @@ const NavBar = () => {
             />
           </Navbar.Brand>
           <Nav>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-            <Nav.Link href="/post">Post</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            {username ? (
+              <>
+                <Nav.Link href="/create">Post Create</Nav.Link>
+                <Nav.Link onClick={handleLogoutChange}>Log out</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login">Log in</Nav.Link>
+                <Nav.Link href="/register">Register User</Nav.Link>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
