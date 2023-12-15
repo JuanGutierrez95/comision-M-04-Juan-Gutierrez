fcbbb48091e9d9eaeae9102fc094fdf62e211eb7
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-15 00:12:42 -0300

    Carga y visualización de listado en la vista Home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,10 +1,34 @@
 import { Card } from "react-bootstrap";
+import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import DateTable from "../components/DateTable";
+import { useAuthContext } from "../context/AuthContext";
 
 const Home = () => {
+  const [list, setList] = useState([]);
+  const { username } = useAuthContext();
+
+  const loadList = async () => {
+    const url = "http://localhost:3000/posts";
+
+    let response = await fetch(url);
+
+    if (response.status === 200) {
+      response = await response.json();
+    }
+    setList(response);
+  };
+
+  useEffect(() => {
+    loadList();
+  }, []);
   return (
+    <>
     <Card.Body>
-      <p>Home</p>
+      {username ? ("Welcome " + username + "!") : "No se inició sesión."}
+      <DateTable list={list} username={username} />
     </Card.Body>
+    </>
   );
 };
 
