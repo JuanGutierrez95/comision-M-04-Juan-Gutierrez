89b23b8ba308a38d9f02a84cdc371164896228b5
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-14 23:49:24 -0300

    Añadidas rutas con React Router

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
+import RegisterUser from "../views/RegisterUser";
+import PostCreate from "../views/PostCreate";
+import Eliminate from "../views/Eliminate";
+import Edit from "../views/Edit";
+import View from "../views/View";
+import Login from "../views/Login";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      
+      <Route path="/register" element={<RegisterUser />}></Route>
+      <Route path="/create" element={<PostCreate />}></Route>
+      <Route path="/eliminate/:id" element={<Eliminate />}></Route>
+      <Route path="/edit/:id" element={<Edit />}></Route>
+      <Route path="/view/:id" element={<View />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 };
