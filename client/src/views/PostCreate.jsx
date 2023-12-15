4efabb5d63603d496c0d0cce759ede9fca9d0d11
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-14 23:57:42 -0300

    Agregado componente PostCreate para la creación de publicaciones

---
+++ client/src/views/PostCreate.jsx
@@ -0,0 +1,11 @@
+import PostCreateForm from "../Form/PostCreateForm";
+import { Card } from "react-bootstrap";
+const PostCreate = () => {
+  return (
+    <Card.Body>
+      <PostCreateForm />
+    </Card.Body>
+  );
+};
+
+export default PostCreate;
