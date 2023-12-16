e54a4a93e65e262a19e39697241f4c475180566d
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-15 23:26:20 -0300

    Se trabajó el componente Crear Posteo

---
+++ client/src/views/CrearPosteo.jsx
@@ -0,0 +1,12 @@
+import { Card } from "react-bootstrap";
+import FormularioCrearPosteo from "../Form/FormularioCrearPosteo.jsx";
+
+const CrearPosteo = () => {
+  return (
+    <Card.Body>
+      <FormularioCrearPosteo />
+    </Card.Body>
+  );
+};
+
+export default CrearPosteo;
