6f7bed52a8ac8e54e0b8101f312932fb2521d05c
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-15 23:42:06 -0300

    Se trabajó en el componente AppRouter

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../views/Home";
-import RegisterUser from "../views/RegisterUser";
-import PostCreate from "../views/PostCreate";
-import Eliminate from "../views/Eliminate";
-import Edit from "../views/Edit";
-import View from "../views/View";
+import Inicio from "../views/Inicio";
+import RegistrarUsuario from "../views/RegistrarUsuario";
+import CrearPosteo from "../views/CrearPosteo";
+import Eliminar from "../views/Eliminar";
+import Editar from "../views/Editar";
+import Ver from "../views/Ver";
 import Login from "../views/Login";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<RegisterUser />}></Route>
-      <Route path="/create" element={<PostCreate />}></Route>
-      <Route path="/eliminate/:id" element={<Eliminate />}></Route>
-      <Route path="/edit/:id" element={<Edit />}></Route>
-      <Route path="/view/:id" element={<View />}></Route>
-      <Route path="/login" element={<Login />}></Route>
+      <Route path="/" element={<Inicio />} />
+      <Route path="/register" element={<RegistrarUsuario />} />
+      <Route path="/crear" element={<CrearPosteo />} />
+      <Route path="/eliminar/:id" element={<Eliminar />} />
+      <Route path="/editar/:id" element={<Editar />} />
+      <Route path="/ver/:id" element={<Ver />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Inicio />} />
     </Routes>
   );
 };
