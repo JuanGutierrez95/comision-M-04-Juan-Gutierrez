62ce591990949ebfd61501b8575d8bfd2293de76
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-12-15 23:24:31 -0300

    Se trabajó el componente Registrar Usuario

---
+++ client/src/views/RegistrarUsuario.jsx
@@ -0,0 +1,12 @@
+import { Card } from "react-bootstrap";
+import FormularioRegistrarUsuario from "../Form/FormularioRegistrarUsuario";
+
+const RegistrarUsuario = () => {
+  return (
+    <Card.Body>
+      <FormularioRegistrarUsuario />
+    </Card.Body>
+  );
+};
+
+export default RegistrarUsuario;
